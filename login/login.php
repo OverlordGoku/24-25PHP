53c14ea17a0bf117e289c65d22527cc4d33aca30
12862 <daniel.gosses@vtsd.com>
2025-01-22 07:47:04 -0500

    24-25 PHP

---
+++ login/login.php
@@ -0,0 +1,12 @@
+<html>
+<head>
+    <title>Login Form</title>
+</head>
+<body>
+<form name="login" method="post" action="action.php">
+    Username: <input type="text" name="user"><br />
+    Password: <input type="password" name="pass"><br />
+    <input type="submit">
+</form>
+</body>
+</html>
